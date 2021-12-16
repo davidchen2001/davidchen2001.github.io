70a7ca74a192c193de2fb3ed06c9225fec82f175
Dioceus <chuwei.chen2001@gmail.com>
2021-12-16 00:51:10 -0500

    add image and icons

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
-import {Grid, Card} from '@mui/material';
+import {Avatar, Grid, Button, IconButton} from '@mui/material';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import BookIcon from '@mui/icons-material/Book';
 import { introduction } from './data';
+import portrait from "../../assets/image.jpg"
 
 export default function AboutMe() {
     return (
         <div>
-            <Grid container justify = 'center' alignItems= 'center' >
-                <Grid item xs = {6}>
-                    <Card>
-                        
-                    </Card>
+            <Grid container justify = 'center'>
+                <Grid container item xs = {6} >
+                    <Grid item>
+                        <Avatar src = {portrait} style = {{left: "50%", top: "50%", width: "50%", height: "50%", borderRadius: "5%"}}/>
+                    </Grid>
+                    
+                    <Grid container>
+                        <IconButton>
+                            <EmailIcon />
+                        </IconButton>
+
+                        <IconButton>
+                            <GitHubIcon />
+                        </IconButton>
+
+                        <IconButton>
+                            <LinkedInIcon />
+                        </IconButton>
+
+                        <Button variant = "contained" size = "small">
+                            Blog
+                        </Button>
+
+                    </Grid>
                 </Grid>
                 <Grid item xs = {6}>
-                        
+                    
                 </Grid>
             </Grid>
         </div>
