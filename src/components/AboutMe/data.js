6be671360201df34dba52a29907ac0053781ccab
Dioceus <chuwei.chen2001@gmail.com>
2021-12-22 00:26:26 -0500

    reformat text

--- src/components/AboutMe/data.js
+++ src/components/AboutMe/data.js
@@ -3,12 +3,17 @@ import {Typography} from "@mui/material";
 export const introduction = (
     <div>
         <Typography variant = "h5">
-        Hey there! I'm David.
+        Hey! I'm David.
         </Typography>
+        <br/>
         <Typography variant = "body1">
         I'm currently working as an Embedded Software Engineering Intern at Ciena.
         When I'm not programming or working, I like cooking, watching anime, and bouldering. I also occasionally maintain a blog. 
-        I'd love to connect with you! Feel free to reach out on one of the following platforms. 
         </Typography>
+        <br/>
+        <Typography>
+        I'm always down to chat! Please feel free to reach out. 
+        </Typography>
+        <br/>
     </div>
 );
