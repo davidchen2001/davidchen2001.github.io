1ea63e45c444c9fe3134e1417242397ff767104f
Dioceus <chuwei.chen2001@gmail.com>
2021-12-21 19:00:00 -0500

    remove background video (too distracting)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,9 +7,6 @@ import "./Home.css"
 export default function Home() {
     return (
         <div className='home'>
-            <video className='showcase-video' autoPlay loop muted>
-                <source src={WallPaper} type='video/mp4' />
-            </video>
             <Main />
         </div>
     );
