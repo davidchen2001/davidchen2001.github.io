cc0d5dec286860a1d4e146a91a966a9c090dcc06
Dioceus <chuwei.chen2001@gmail.com>
2021-12-23 22:53:10 -0500

    add Background

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Main from "../Main/Main";
+import Background from "../Background/Background";
 import "./Home.css";
 
 export default function Home() {
 
     return (
         <div className='home'>
+            <Background />
             <Main />
         </div>
     );
