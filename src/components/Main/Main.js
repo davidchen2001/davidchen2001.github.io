01ba8af31c0976ae803ce3580ffc8d692308558b
Dioceus <chuwei.chen2001@gmail.com>
2022-01-09 15:11:13 -0500

    avatar position and text responsive

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -13,14 +13,14 @@ export default function Main() {
   return (
     <div className="main-container">
       <Grid container justifyContent="center" alignItems="center" spacing = {4}>
-        <Grid item xs = {12} sm = {12} md = {12} lg = {6} justifyContent="center" alignItems = "center">
+        <Grid item xs = {12} sm = {12} md = {12} lg = {6} className = "avatar-container">
             <Avatar
               src={portrait}
-              style={{ width: "40vh", height: "40vh", borderRadius: "50%"}}
+              style={{ width: "40vh", height: "40vh", borderRadius: "50%", objectFit: "cover", overflow: "hidden"}}
             />
         </Grid> 
 
-        <Grid item xs = {6} sm = {12} md = {12} lg = {6} >
+        <Grid item xs = {6} sm = {12} md = {12} lg = {6} className = "about-position">
           <About />
         </Grid>
       </Grid>
