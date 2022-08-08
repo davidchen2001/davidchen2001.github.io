f9107b72f6e315224b4915d14a14da9955f5d1bc
Dioceus <chuwei.chen2001@gmail.com>
2022-08-07 19:45:46 -0700

    update resume

--- src/components/About/data.js
+++ src/components/About/data.js
@@ -9,7 +9,7 @@ export const links = {
     github: "https://github.com/davidchen2001",
     linkedin: "https://www.linkedin.com/in/david-chuwei-chen/",
     blog: "blog",
-    resume: "https://drive.google.com/file/d/1gbgg8dWZ6E4DVT38eXqAbOhHcOw9fL2p/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1bMML3DN2LWO8SBA2mXjMf76t5enm5L3p/view?usp=sharing",
     ciena: "https://www.ciena.com/",
     blueprint: "https://cublueprint.org/",
     beneficent: "https://beneficent.cc/",
